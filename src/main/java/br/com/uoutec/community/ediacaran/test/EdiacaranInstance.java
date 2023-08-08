cf30c425d3d6fc6918c2ea46c4057401d307cbae
Brandao <Brandao@git.uoutec.com.br>
2023-08-08 20:11:08 -0300

    default

---
+++ src/main/java/br/com/uoutec/community/ediacaran/test/EdiacaranInstance.java
@@ -0,0 +1,169 @@
+package br.com.uoutec.community.ediacaran.test;
+
+import java.beans.XMLDecoder;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import br.com.uoutec.community.ediacaran.EdiacaranBootstrap;
+import br.com.uoutec.community.ediacaran.PluginManager;
+import br.com.uoutec.community.ediacaran.plugins.PluginInitializer;
+import br.com.uoutec.io.resource.DefaultResourceLoader;
+import br.com.uoutec.io.resource.Resource;
+import br.com.uoutec.io.resource.ResourceLoader;
+
+public class EdiacaranInstance {
+
+	private EdiacaranBootstrap ediacaranBootstrap;
+	
+	private PluginManager pluginManager;
+	
+	public void destroy() {
+		if(ediacaranBootstrap != null) {
+			ediacaranBootstrap.stopApplication();
+		}
+	}
+	
+	public void start(Class<?> testClass) throws Throwable{
+		
+		ResourceLoader resourceLoader = new DefaultResourceLoader();
+		
+		String config = getConfigPath(testClass);
+		
+		InputStream in = null;
+		try {
+			Resource resource = resourceLoader.getResource(config);
+			
+			if(resource == null) {
+				throw new RuntimeException("not found: " + config);
+			}
+			
+			in = resource.getInputStream();
+			XMLDecoder xml          = new XMLDecoder(in);
+			this.ediacaranBootstrap = (EdiacaranBootstrap) xml.readObject();
+			xml.close();
+		}
+		catch(IOException ex) {
+			throw new RuntimeException(ex);
+		}
+		finally {
+			try {
+				if(in != null) 
+					in.close();
+			}
+			catch(Throwable ex) {
+			}
+		}
+		
+
+		Map<String,Object> params = getParameters(testClass);
+		
+		applyDefaultConfiguration(params);
+		
+		ediacaranBootstrap.loadApplication(params);
+		ediacaranBootstrap.startApplication();
+		
+		this.pluginManager = ediacaranBootstrap.getPluginManager();
+	}
+	
+	public Object execute(Callable<Object> value, String context) throws Throwable {
+	
+		Map<String,Object> contextVars = getPluginConfigVars(this.pluginManager, context);
+		ClassLoader classLoader = null;
+		
+		if(contextVars != null) {
+			classLoader = (ClassLoader)contextVars.get(PluginInitializer.CLASS_LOADER);
+		}
+		 
+    	ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
+    	
+		if(contextVars != null) {
+			Thread.currentThread().setContextClassLoader(classLoader);
+		}
+    	
+    	try {
+    		return value.call();
+    	}
+    	finally {
+    		if(contextVars != null) {
+    			Thread.currentThread().setContextClassLoader(oldClassLoader);
+    		}
+    	}
+    	
+	}
+	
+	private String getConfigPath(Class<?> testClass) {
+		ApplicationConfigTest config = testClass.getDeclaredAnnotation(ApplicationConfigTest.class);
+		return config == null? "ediacaran/config/ediacaran-config.xml" : config.value();
+	}
+
+	private Map<String,Object> getParameters(Class<?> testClass) {
+		
+		Map<String,Object> r = new HashMap<String,Object>();
+		
+		ApplicationConfigParametersTest params = testClass.getDeclaredAnnotation(ApplicationConfigParametersTest.class);
+		
+		if(params != null) {
+			for(ApplicationConfigParameterTest p: params.value()) {
+				r.put(p.paramName(), p.paramValue());
+			}
+		}
+		
+		ApplicationConfigParameterTest param = testClass.getDeclaredAnnotation(ApplicationConfigParameterTest.class);
+
+		if(param != null) {
+			r.put(param.paramName(), param.paramValue());
+		}
+		
+		return r;
+	}
+
+	private void applyDefaultConfiguration(Map<String, Object> contextParams) throws MalformedURLException {
+		
+			if(!contextParams.containsKey("app")) {
+					contextParams.put(
+						"app",
+						"ediacaran" + File.separator + 
+						"config" + File.separator +
+						"ediacaran-config.xml"
+					);
+			}
+			
+			if(!contextParams.containsKey(EdiacaranBootstrap.CONFIG_FILE_VAR)) {
+				contextParams.put(
+					EdiacaranBootstrap.CONFIG_FILE_VAR, 
+					new File(System.getProperty("user.dir") + File.separator + 
+					"ediacaran" + File.separator + 
+					"config" + File.separator +
+					"ediacaran-dev.properties").toURI().toURL().toExternalForm()
+				);
+			}
+			
+			if(!contextParams.containsKey(EdiacaranBootstrap.LOGGER_CONFIG_FILE_VAR)) {
+				contextParams.put(
+					EdiacaranBootstrap.LOGGER_CONFIG_FILE_VAR, 
+					new File(System.getProperty("user.dir") + File.separator + 
+					"ediacaran" + File.separator + 
+					"config" + File.separator +
+					"log4j.configuration").toURI().toURL().toExternalForm()
+				);
+			}
+		
+			if(!contextParams.containsKey(EdiacaranBootstrap.BASE_PATH_PROPERTY)) {
+				contextParams.put(
+					EdiacaranBootstrap.BASE_PATH_PROPERTY, 
+					"ediacaran" + File.separator 
+				);
+			}
+				
+	}
+	
+	private Map<String,Object> getPluginConfigVars(PluginManager pluginManager, String context){
+		return context == null? null : pluginManager.getPluginConfigVars(context);
+	}
+
+}
---
+++ src/main/java/br/com/uoutec/community/ediacaran/test/junit5/EdiacaranJUnit.java
@@ -0,0 +1,129 @@
+package br.com.uoutec.community.ediacaran.test.junit5;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.InvocationInterceptor;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
+import org.junit.jupiter.api.extension.TestInstancePostProcessor;
+
+import br.com.uoutec.community.ediacaran.plugins.EntityContextPlugin;
+import br.com.uoutec.community.ediacaran.test.EdiacaranInstance;
+import br.com.uoutec.community.ediacaran.test.PluginContext;
+
+public class EdiacaranJUnit 
+	implements TestInstancePostProcessor, 
+	InvocationInterceptor, BeforeAllCallback, AfterAllCallback, 
+	BeforeEachCallback, AfterEachCallback, ParameterResolver {
+
+	private EdiacaranInstance ediacaran;
+	
+	public EdiacaranJUnit() {
+		ediacaran = new EdiacaranInstance();
+	}
+	
+	@Override
+	public void postProcessTestInstance(Object testInstance, ExtensionContext context) throws Exception {
+	}
+
+
+	 public void interceptTestMethod(Invocation<Void> invocation,
+	            ReflectiveInvocationContext<Method> invocationContext,
+	            ExtensionContext extensionContext) throws Throwable {
+
+		String context = 
+				getContextName(
+						invocationContext.getTargetClass(), 
+						invocationContext.getExecutable().getAnnotation(PluginContext.class)
+				);
+
+		ediacaran.execute(()->{
+			try {
+				return invocation.proceed();
+			}
+			catch(Throwable ex) {
+				throw new Exception(ex);
+			}
+		}, context);
+    }
+
+
+	@Override
+	public void afterEach(ExtensionContext context) throws Exception {
+	}
+
+
+	@Override
+	public void beforeEach(ExtensionContext context) throws Exception {
+	}
+
+
+	@Override
+	public void afterAll(ExtensionContext context) throws Exception {
+		ediacaran.destroy();
+	}
+
+
+	@Override
+	public void beforeAll(ExtensionContext context) throws Exception {
+		try {
+			ediacaran.start(context.getTestClass().get());
+		}
+		catch(Throwable ex) {
+			throw new Exception(ex);
+		}
+	}
+
+
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+			throws ParameterResolutionException {
+		//return parameterContext.getParameter().getType().equals(EmployeeJdbcDao.class);
+		return getContextName(
+				parameterContext.getDeclaringExecutable().getDeclaringClass(), 
+				parameterContext.getDeclaringExecutable().getAnnotation(PluginContext.class)
+		) != null;
+	}
+
+
+	@Override
+	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+			throws ParameterResolutionException {
+		
+		Class<?> type = parameterContext.getParameter().getType();
+		
+		String context = 
+				getContextName(
+						parameterContext.getDeclaringExecutable().getDeclaringClass(), 
+						parameterContext.getDeclaringExecutable().getAnnotation(PluginContext.class)
+				);
+		
+		try {
+			return ediacaran.execute(()->{
+				Class<?> contextType = Thread.currentThread().getContextClassLoader().loadClass(type.getName());
+				return EntityContextPlugin.getEntity(contextType);
+			}, context);
+		}
+		catch(Throwable ex) {
+			throw new ParameterResolutionException("parameter error", ex);
+		}
+		
+	}
+	
+	private String getContextName(Class<?> clazz, PluginContext context){
+		
+		if(context == null && clazz.isAnnotationPresent(PluginContext.class)) {
+			context = clazz.getDeclaredAnnotation(PluginContext.class);
+		}
+	
+		return context == null? null : context.value();
+	}
+	
+}
